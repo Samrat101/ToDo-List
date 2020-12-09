7e2882a225fb8a1ec4893907ab5a5667bdd249c7
Samrat101 <samratrao101@gmail.com>
2020-12-09 15:19:03 +0530

    Custom Lists Added

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose =require('mongoose');
+const mongoose = require('mongoose');
+const _ = require('lodash');
 const app = express();
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
-
 
 mongoose.connect('mongodb://localhost:27017/ToDoDb', {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -51,26 +51,60 @@ app.get('/', function(req, res){
 
 app.post('/',function(req,res){
   const itemname = req.body.newItem;
+  const listName = req.body.list;
+
   const item = new Item({
     name:itemname
   })
-  item.save();
-  res.redirect('/');
+
+  if(listName === "Today"){
+    item.save();
+    res.redirect('/');
+  }else{
+    List.findOne({name:listName},function(err,foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/"+listName);
+    });
+  }
 });
 
 app.post('/delete',function(req,res){
   const checkeditemID = req.body.checkbox;
-  Item.findByIdAndRemove(checkeditemID,function(err){
-    if(!err){
-      console.log("Deleted checked item successfully");
-      res.redirect('/');
-    }
-  });
+  const listName = req.body.listName;
+
+  if(listName === "Today"){
+    Item.findByIdAndRemove(checkeditemID,function(err){
+      if(!err){
+        console.log("Deleted checked item successfully");
+        res.redirect('/');
+      }
+    });
+  }else{
+      List.findOneAndUpdate({name:listName},{ $pull : { items : { _id : checkeditemID } } },function(err,foundList){
+        if(!err){
+          res.redirect("/"+listName);
+        }
+      });
+  }
 });
 
 app.get("/:customListName",function(req,res){
-  const customListName = req.params.customListName;
-
+  const customListName = _.capitalize(req.params.customListName);
+  List.findOne({name:customListName},function(err,foundList){
+    if(!err){
+      if(!foundList){
+        const list = new List({
+           name:customListName,
+           items:defaultitems
+        });
+        list.save();
+        res.redirect("/"+customListName);
+      }else{
+        res.render("list",{Listtitle:foundList.name,newListItems:foundList.items});
+      }
+    }
+  });
 });
 
 app.listen(3000,function(req,res){
